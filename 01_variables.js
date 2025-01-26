e390ff32c910b7b53f87a9f85dca81fbaea632c6
shammichaudhari <shammi.chaudhari@gmail.com>
2025-01-26 14:10:29 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const userId = 852639
+let userEmail = "shammi.chaudhari@gmail.com"
+var userPassword = "1235User"
+userCity = "Bardoli"
+
+// userId = 85
+console.log(userId)
+
+userEmail = "hsb@gmail.com"
+userPassword = "Pwd@456"
+userCity = "Surat"
+
+console.table([userId, userEmail, userPassword, userCity])
---
+++ 06_number_maths.js
@@ -0,0 +1,32 @@
+const score = 400
+
+const balance  = new Number(500)
+
+console.log(balance.toFixed(2))
+
+// console.log(balance.toString())
+
+const otherNumber = 56.89566
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreads = 1000000
+
+// console.log(hundreads.toLocaleString('en-IN'))
+
+/******    MATH OPERATIONS ***********/
+
+// console.log(Math.random())
+
+// console.log(Math.floor((Math.random()*10)) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
